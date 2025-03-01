cee3d066b195bfc84319d2eb4664817de4aa3d0d
DinosMpo <dinos7591@yahoo.gr>
2025-03-01 17:53:09 +0200

    init project

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+import * as d3 from "d3";
+
+function App() {
+  const test = d3;
+
+  console.log(test);
+  return (
+    <div>
+      <h1 id="title">Data Chart</h1>
+
+    </div>
+  )
+}
+
+export default App
