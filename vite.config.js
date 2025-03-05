502f4dd8d07f13a811bc3b270bd9f0682963e076
DinosMpo <dinos7591@yahoo.gr>
2025-03-05 12:29:23 +0200

    changes

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://github.com/DinosMpo/freecodecamp-data-chart-bar"
+  base: "/freecodecamp-data-chart-bar/"
 })
